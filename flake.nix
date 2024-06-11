3671f5f116c4fba04dbcd98572d499868b0df038
TJ <tj@wilder.tj>
2024-06-10 20:02:55 -0500

    Fix home manager

--- flake.nix
+++ flake.nix
@@ -4,16 +4,24 @@
   inputs = {
     # NixOS official packages source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    home-manager.url = "path:./home-manager/flake.nix";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      _modules.args = { inherit home-manager; };
       modules = [
 	# Load the default configuration.nix
         ./configuration.nix
+	home-manager.nixosModule.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.userUserPackages = true;
+	  home-manager.users.tj = import ./home.nix;
+	  # Optionally use home-manager.extraSpecialArgs to pass
+	  # arguments to home.nix
+	}
       ];
     };
   };
