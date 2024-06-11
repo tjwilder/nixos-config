f71a0047ebf82112c0a8b49151c4816985e26aae
TJ <tj@wilder.tj>
2024-06-10 20:11:45 -0500

    Fix home manager version

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # NixOS official packages source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   
@@ -18,7 +18,14 @@
 	{
 	  home-manager.useGlobalPkgs = true;
 	  home-manager.useUserPackages = true;
-	  home-manager.users.tj = import ./home.nix;
+	  home-manager.users.tj = { pkgs, ... }: {
+		  home.packages = [ pkgs.atool pkgs.httpie ];
+		  #programs.bash.enable = true;
+
+		  # The state version is required and should stay at the version you
+		  # originally installed.
+		  home.stateVersion = "24.05";
+		};
 	  # Optionally use home-manager.extraSpecialArgs to pass
 	  # arguments to home.nix
 	}
