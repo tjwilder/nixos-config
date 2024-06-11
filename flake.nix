e7de3c17f091319e20f1f3f2cdb31c233fb7dc90
TJ <tj@wilder.tj>
2024-06-10 19:28:47 -0500

    Initial

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Basic NixOS flake";
+
+  inputs = {
+    # NixOS official packages source
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+  
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+	# Load the default configuration.nix
+        ./configuration.nix
+      ];
+    };
+  };
+}
