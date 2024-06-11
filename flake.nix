defa0204310d047401cd0d438ba0a94e3de8d3fc
TJ <tj@wilder.tj>
2024-06-10 19:51:51 -0500

    Bump

--- flake.nix
+++ flake.nix
@@ -4,15 +4,16 @@
   inputs = {
     # NixOS official packages source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager.url = "path:./home-manager/flake.nix";
   };
   
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      _modules.args = { inherit home-manager; };
       modules = [
 	# Load the default configuration.nix
         ./configuration.nix
-	./home-manager/flake.nix
       ];
     };
   };
